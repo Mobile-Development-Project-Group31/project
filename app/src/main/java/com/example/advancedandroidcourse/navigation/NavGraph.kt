249bdf24a2530abd718139b08ebc75a810a55f58
t3jehe00 <t3jehe00@students.oamk.fi>
2025-03-25 11:36:50 +0200

    basic setup for firebase (follow advanced mobile programming lecture 1)

---
+++ app/src/main/java/com/example/advancedandroidcourse/navigation/NavGraph.kt
@@ -0,0 +1,4 @@
+package com.example.advancedandroidcourse.navigation
+
+//Navigation with Jetpack Navigation Component
+// THIS IS NOT USED IN THIS DEMO APP, because the demo has only one view
---
+++ app/src/main/java/com/example/advancedandroidcourse/presentation/auth/LoginScreen.kt
@@ -0,0 +1,3 @@
+package com.example.advancedandroidcourse.presentation.auth
+
+// THIS IS NOT USED IN THIS DEMO APP, because the demo has only one view
